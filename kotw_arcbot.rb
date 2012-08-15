8c819fb6d4f774f45a74d6a2c495c848e3cfae02
Jeff Peterson <jeff@petersonj.com>
2012-08-15 14:31:56 -0400

    bug fix

--- kotw_arcbot.rb
+++ kotw_arcbot.rb
@@ -9,6 +9,7 @@ end
 
 match /^!kotw +(?<word>.+)$/ do
   scores = store('kotw:word').zrevrange(word, 0, 2, with_scores: true)
+  return if scores.nil?
   scores.each do |tuple|
     who, score = tuple
     reply "#{who} has said '#{word}' #{'time'.pluralize(score)}.", false
