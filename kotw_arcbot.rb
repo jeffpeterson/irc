ffacefcc7cd1ab602ee915f082c543f6339ad394
Jeff Peterson <jeff@petersonj.com>
2012-08-15 16:26:53 -0400

    normalize input

--- kotw_arcbot.rb
+++ kotw_arcbot.rb
@@ -1,14 +1,19 @@
 require 'irc'
 
+def norm word
+  word.gsub(/(^[.!?]+)|([!?.]+$)/i,'').downcase
+end
+
 match /^(?!!kotw)(?<everything>.+)/ do
   words = everything.split(/ +/)
   words.each do |word|
-    word = word.gsub(/(^[.!?]+)|([!?.]+$)/i,'').downcase
+    word = norm(word)
     store('kotw:word').zincrby word, 1, (user || nick)
   end
 end
 
 match /^!kotw +(?<word>.+)$/ do
+  self.word = norm(word)
   scores = store('kotw:word').zrevrange(word, 0, 0, with_scores: true)
   reply("I haven't seen anybody say '#{word}', yet.") and next if !scores
 
