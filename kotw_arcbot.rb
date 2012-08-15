d10b754ca0d616fbf06e9c1b456aaab94b1cfb3b
Jeff Peterson <jeff@petersonj.com>
2012-08-15 16:32:05 -0400

    bug fixes

--- kotw_arcbot.rb
+++ kotw_arcbot.rb
@@ -8,16 +8,19 @@ match /^(?!!kotw)(?<everything>.+)/ do
   words = everything.split(/ +/)
   words.each do |word|
     word = norm(word)
-    store('kotw:word').zincrby word, 1, (user || nick)
+    store('kotw:word').zincrby word, 1, nick
   end
 end
 
-match /^!kotw +(?<word>.+)$/ do
+match /^!kotw +(?<word>[^ ]+)/ do
   self.word = norm(word)
   scores = store('kotw:word').zrevrange(word, 0, 0, with_scores: true)
-  reply("I haven't seen anybody say '#{word}', yet.") and next if !scores
 
-  king, score = scores.first
+  if !scores
+    reply("I haven't seen anybody say '#{word}', yet.")
+  else
+    king, score = scores.first
 
-  reply "#{king} is the king of '#{word}' (#{score.to_i}).", false
+    reply "#{king} is the king of '#{word}' (#{score.to_i}).", false
+  end
 end
