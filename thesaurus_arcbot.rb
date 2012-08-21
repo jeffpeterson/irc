e337d1cca07e92577ce4bb86dcf4dfc91224dbe3
Jeff Peterson <jeff@petersonj.com>
2012-08-20 20:52:23 -0400

    fix redis error

--- thesaurus_arcbot.rb
+++ thesaurus_arcbot.rb
@@ -8,7 +8,10 @@ def lookup word
   return synonyms if synonyms = store('thesaurus:synonyms').smembers(word)
 
   if synonyms = fetch_word(word)
-    store('thesaurus:synonyms').sadd(word, synonyms)
+    synonyms.each do |s|
+      store('thesaurus:synonyms').sadd(word, s)
+    end
+
     synonyms
   end
 end
